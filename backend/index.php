ee1268d8caa12964151b38b7db8949315a222bf9
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2021-01-14 17:22:52 -0300

    updated directory structure

--- backend/index.php
+++ backend/index.php
@@ -1,12 +1,12 @@
 <?php
 	# includes
-	require_once __DIR__ . '/ENV.php';
+	require_once __DIR__ . '/env.php';
 
 	# vars
-	$uri = __DIR__ . '/src' . $_SERVER['REQUEST_URI'] . '/' . $_SERVER['REQUEST_METHOD'] . '.php';
+	$uri = __DIR__ . $_SERVER['REQUEST_URI'] . '/' . $_SERVER['REQUEST_METHOD'] . '.php';
 
 	# CORS
-	header('Access-Control-Allow-Origin: ' . CORS);
+	header('Access-Control-Allow-Origin: ' . getenv('CORS'));
 
 	if (file_exists($uri))
 		require_once $uri;
