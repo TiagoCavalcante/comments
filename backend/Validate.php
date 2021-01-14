a861a539dd9f2b6b6ee81119fed66e5ebdb72327
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2021-01-14 17:25:48 -0300

    moved src/Validate.php to Validate.php

---
+++ backend/Validate.php
@@ -0,0 +1,26 @@
+<?php
+	abstract class Validate {
+		# vars
+		public $value;
+	}
+
+	class ValidateString extends Validate {
+		# constructor
+		function __construct(string $value) {
+			$this->value = $value;
+		}
+
+		# functions
+		function min(int $value) : bool {
+			return (strlen($this->value) >= $value) ? true : false;
+		}
+
+		function max(int $value) : bool {
+			return (strlen($this->value) <= $value) ? true : false;
+		}
+
+		function length(int $value) : bool {
+			return (strlen($this->value) == $value) ? true : false;
+		}
+	}
+?>
